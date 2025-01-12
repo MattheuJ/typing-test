383f94ee2ae450293c88ac71ce7d783a8c2f3ef6
MattheuJ <mattheujimenez@gmail.com>
2025-01-11 17:01:34 -0800

    Created CSS, HTML, and JS files. Put down boiler plate code.

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const inputField = document.getElementById("testInput");
+
+inputField.addEventListener("keydown", function(event) {
+
+  console.log("Key pressed:", event.key); // Prints the pressed key
+
+  // Access the input field value using 'this.value' if needed
+
+});
